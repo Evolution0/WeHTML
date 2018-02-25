fd62c404c5f988a130bf60ea6a3e554dabf0526f
Anthony Forsberg <forsberganthony@yahoo.com>
2018-02-25 11:00:41 -0500

    Update Syntax, use PROGMEM, Simplify
    Updated too Python 3+, PEP8-ified
    Switch to using PROGMEM
    Simplified the code a bit

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # WeHTML
 A simple tool for converting HTML code to code usable with the Arduino ESP8266WiFi
-
-Also, a note, you can not use " in your HTML code as it will interfer with the Arduino code. 
--- WeHTML.py
+++ WeHTML.py
@@ -1,62 +1,45 @@
 import os
 
-def Start():
-	print ""
-	print "WeHTML initially made by Aidan Welch"
-	print ""
 
-	print("This is a tool for converting HTML code into code usable with Wemos.")
-	raw_input("Press Enter(Return) to continue...")
-	FindFile()
+def start():
+    print("\nWeHTML initially made by Aidan Welch\n")
+    print("This is a tool for converting HTML code into code usable with Wemos.")
+    input("Press Enter(Return) to continue...")
+    find_file()
 
 
-def FindFile():
-	location = raw_input("Input your file's location: ")
-	if len(location) < 1:
-		print ("You didn't type anything")
-		FindFile()
-	else:
-		 #Opens the file and finds it
-		 file = open(location, "r")
-		 print "Following will be the first 4 characters of " + os.path.basename(location) + ":"
-		 print file.read(4)
-		 unanswered = True
-		 while unanswered == True:
-		 	answer = raw_input("Type Y if correct or N if false: ")
-		 	if answer.upper() == "Y":
-		 		GetHTML(location)
-		 		unanswered = False
-		 	elif answer.upper() == "N":
-		 		FindFile()
-		 		unanswered = False
-		 	else:
-		 		unanswered = True
+def find_file():
+    location = input("Input your file's location: ")
+    if len(location) < 1:
+        print("You didn't type anything")
+        find_file()
+    else:
+        # Opens the file and confirms if its correct
+        with open(location) as file:
+            print("Following will be the first 4 characters of {0}:".format(os.path.basename(location)))
+            print(file.read(4))
+            while True:
+                answer = input("Type Y if correct or N if false: ")
+                if answer.upper() == "Y":
+                    get_html(location)
+                    break
+                elif answer.upper() == "N":
+                    find_file()
+                    break
+                else:
+                    continue
 
 
-def file_len(location):  #credit for this goes to https://www.safaribooksonline.com/library/view/python-cookbook/0596001673/ch04s07.html
-	num_lines = 0
-	for line in open(location).xreadlines( ):
-		num_lines += 1
-	print("I counted " + str(num_lines) + " lines")
-	return(num_lines)
+def get_html(location):
+    # must reopen file because every time file is read it saves cursor position
+    template = """#define USE_PROGMEM 1\nconst char* webPage PROGMEM = R"\n{1}";\n{0}.send(200, "text/html", webPage)"""
+    new_file = input("Where should the new file(include the name for the file in the path) be saved: ")
+    with open(location) as file, open(new_file, "w+") as output_file:
+        client = input("What is the name of the variable holding your WI-FI client: ")
+        output_file.write(template.format(client, file.read()))
 
-def GetHTML(location):
-	#must reopen file because everytime file is read it saves cursor position
-	lineCount = file_len(location)
-	file = open(location, "r")
-	newfile = open(raw_input("Where should the new file(include the name for the file in the path) be saved: "), "w")
+        print("Done!")
 
-	client = raw_input("What is the name of the variable holding your WI-FI client: ")
-	i = 0
- 	while i < lineCount:
- 		print(i)
- 		if i == lineCount - 1:
- 			newfile.write(client + ".println(\"" + file.readline() + "\"); \n")
- 		else:
- 			newfile.write(client + ".println(\"" + file.readline()[:-1] + "\"); \n")
- 		i += 1
 
- 	print("Done!")
-	unanswered = False
-
-Start() #starting Start()
+if __name__ == '__main__':
+    start()
